4753df6989c3638af41b319105e11a745152e5bc
Ambuz Ranjan <ambuzrnjn33@yahoo.com>
2019-10-24 00:21:57 +0530

    First commit

---
+++ public/chat.js
@@ -0,0 +1,18 @@
+var socket = io.connect("http://localhost:4000");
+
+var message = document.getElementById('message');
+var handle = document.getElementById('handle');
+var btn = document.getElementById('send');
+var output = document.getElementById('output');
+var feedback = document.getElementById('feedback');
+
+btn.addEventListener('click',function(){
+  socket.emit('chat',{
+    message: message.value,
+    handle: handle.valuewB  
+  })
+});
+
+socket.on('chat',function(data){
+  output.innerHTML += "<p><strong>"+ data.handle + ":</strong>" + data.message +"</p>";
+});
